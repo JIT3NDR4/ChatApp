efc507074300bbbcb42b33be1812ed3256c9a3c5
jitendra <jitendra_stark@hotmail.com>
2017-06-30 23:17:17 +0530

    added chat function

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,13 +1,14 @@
-var expect=require('expect');
+var expect = require('expect');
 
-var {generateMessage} =require('./message');
-describe('generateMessage',()=>{
-    it('Should generate correct message object',()=>{
-        var from='Jen';
-        var text='some message';
-        var message=generateMessage(from,text);
+var {generateMessage} = require('./message');
 
-        expect(message.createdAt).toBeA('number');
-        expect(message).toInclude({from,text});
-    });
+describe('generateMessage', () => {
+  it('should generate correct message object', () => {
+    var from = 'Jen';
+    var text = 'Some message';
+    var message = generateMessage(from, text);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, text});
+  });
 });
